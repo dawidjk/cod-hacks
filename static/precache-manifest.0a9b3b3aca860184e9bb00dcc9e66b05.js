e549abb29392c7aaf62777437c2736b54341a17e
dawidjk <dawidjk2@illinois.edu>
2020-06-30 20:50:34 -0400

    build

---
+++ static/precache-manifest.0a9b3b3aca860184e9bb00dcc9e66b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf47324a6715bfc90822a28f37b717b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b82625049c8b5f45ae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e9f8313b634a79f6c3f",
+    "url": "/static/js/2.08583f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08583f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b82625049c8b5f45ae",
+    "url": "/static/js/main.fb5c3af3.chunk.js"
+  },
+  {
+    "revision": "e74e67748bf421949415",
+    "url": "/static/js/runtime-main.aec48102.js"
+  }
+]);
